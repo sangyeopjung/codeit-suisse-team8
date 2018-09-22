eca65c19c429f96227a5eb7e805cf1f63a445da4
Sang Yeop Jung <syjung812@gmail.com>
2018-09-22 19:43:47 +0800

    hotel2

--- codeitsuisse/routes/hotel.py
+++ codeitsuisse/routes/hotel.py
@@ -20,3 +20,39 @@ def hotel():
         res = 0
     print("My result :{}".format(res))
     return jsonify(answer=res)
+
+
+@app.route('/customers-and-hotel/minimum-camps', methods=['POST'])
+def hotel2():
+    data = request.get_json()
+    print("data sent for evaluation {}".format(data))
+
+    n = len(data)
+
+    data = sorted(data, key=lambda k: k["pos"])
+    pos = [[0 for _ in range(n)] for _ in range(n)]
+
+    for i in range(n):
+        j = i + 1
+        pos[i][i] = 1
+        le = data[i]["pos"] - data[i]["distance"]
+        ri = data[i]["pos"] + data[i]["distance"]
+        while j < n:
+            le = max(le, data[j]["pos"] - data[j]["distance"])
+            ri = min(ri, data[j]["pos"] + data[j]["distance"])
+            if le <= ri:
+                pos[i][j] = 1
+                j += 1
+            else:
+                break
+
+    dp = [100000000 for _ in range(n)]
+    dp[0] = 1
+    for i in range(1, n):
+        for j in range(i):
+            if pos[j + 1][i] == 1:
+                dp[i] = min(dp[i], dp[j] + 1)
+
+    print("My result :{}".format(dp[n - 1]))
+    return jsonify(answer=dp[n - 1])
+
