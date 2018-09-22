4159eb397b37bf40c657d8b4d223362214bbb449
Sang Yeop Jung <syjung812@gmail.com>
2018-09-22 20:25:05 +0800

    prime

--- codeitsuisse/routes/PrimeSum.py
+++ codeitsuisse/routes/PrimeSum.py
@@ -22,6 +22,33 @@ logger = logging.getLogger(__name__)
 #     for ele in l:
 #         addition += ele
 #     return addition
+import math, numpy
+
+
+def generateJs(i,n):
+    j=i**2
+    if j<n:
+        yield j
+
+    while j+i<=n:
+        j+=i
+        yield j
+
+
+def SieveOptimized(n):
+    l = list(range(2, n+1))
+    isPrime = [True] * (n+1)
+    maxChk = math.sqrt(n)
+
+    for q,r in enumerate(l):
+        if r>maxChk:
+            break
+
+        for z in generateJs(r,n):
+           isPrime[z] = False
+
+    return numpy.where(isPrime)[0]
+
 
 
 @app.route('/prime-sum', methods=['POST'])
@@ -31,36 +58,29 @@ def evaluatePrimeSum():
     input = data.get("input")
 
     print(input)
+    primes = SieveOptimized(input + 10)[2:]
 
-    # num = inputValue
-    # primes = possiblePrimeArray(num)
-    # result = []
-    # count = len(primes)-1
-    #
-    # while count >=0:
-    #     if primes[count] == num:
-    #         ans = [primes[count]]
-    #         return jsonify(ans)
-    #     else:
-    #         i = 0
-    #         temp = []
-    #         temp.append(prime)
-    #         while i < len(primes) and primes[i] != prime:
-    #             if addList(temp) == num:
-    #                 return jsonify(temp)
-    #             elif (addList(temp) + primes[i])< num:
-    #                 temp.append(primes[i])
-    #             else:
-    #                 j = i-1
-    #                 while j>=0:
-    #                     if (addList(temp)+primes[i]) > num:
-    #                         temp.remove(min(temp))
-    #                     j-=1
-    #                 temp.append(primes[i])
-    #             i+=1
-    #     count -= 1
+    num = len(primes)
+    found = 0
+    result = []
 
-    result = None
+    if input in primes:
+        result.append(input)
 
+    else:
+        if input % 2 == 1:
+            input -= 3
+            result.append(3)
+
+        for i in range(num):
+            for j in range(i + 1, num):
+                if primes[i] + primes[j] == input:
+                    result.extend([primes[i], primes[j]])
+                    found = 1
+                    break
+            if found:
+                break
+
+    result = list(map(int, result))
     logging.info("My result :{}".format(result))
-    return jsonify(result);
+    return jsonify(result)
