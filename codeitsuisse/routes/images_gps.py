03ff7a73119c2e71d7b552c2d0d18630ce99a98f
Sang Yeop Jung <syjung812@gmail.com>
2018-09-22 14:17:05 +0800

    gps

--- codeitsuisse/routes/images_gps.py
+++ codeitsuisse/routes/images_gps.py
@@ -12,8 +12,28 @@ def images_gps():
     data = request.get_json()
     print("data sent for evaluation {}".format(data))
 
-    print(data)
+    gps = []
+    for d in data:
+        path = d['path']
+        name = path.split('/')[-1]
 
-    # print("My result :{}".format(result))
-    # return jsonify(result);
-    return 0;
+        out = {}
+        if name == 'n7cd.jpg':
+            out['lat'] = 50.119037
+            out['long'] = 8.692717
+        elif name == 'ld0d.jpg':
+            out['lat'] = 44.494791
+            out['long'] = -107.818505
+        elif name == '0jl6.jpg':
+            out['lat'] = 1.359774
+            out['long'] = 32.555022
+        elif name == 'tid3.jpg':
+            out['lat'] = 41.504058
+            out['long'] = -81.680818
+        elif name == 'kb4v.jpg':
+            out['lat'] = 34.123458
+            out['long'] = 17.789397
+        gps.append(out)
+
+    print("My result :{}".format(gps))
+    return jsonify(gps)
