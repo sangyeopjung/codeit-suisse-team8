43c7e99c6228ac3b1204fcbed0853b2ce1fa8b34
Sang Yeop Jung <syjung812@gmail.com>
2018-09-22 15:44:58 +0800

    sort

--- codeitsuisse/routes/tetris.py
+++ codeitsuisse/routes/tetris.py
@@ -8,7 +8,7 @@ logger = logging.getLogger(__name__)
 
 
 @app.route('/tetris', methods=['POST'])
-def images_gps():
+def tetris():
     data = request.get_json()
     print("data sent for evaluation {}".format(data))
 
