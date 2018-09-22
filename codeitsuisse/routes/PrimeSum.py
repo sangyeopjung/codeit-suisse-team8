de86efef9cfdc7313e50f1c3ea92f33f56d2ded4
wonyo <wonyoung1026@hotmail.com>
2018-09-22 21:18:38 +0900

    prime

--- codeitsuisse/routes/PrimeSum.py
+++ codeitsuisse/routes/PrimeSum.py
@@ -5,23 +5,23 @@ from codeitsuisse import app;
 logger = logging.getLogger(__name__)
 
 
-# def possiblePrimeArray(num):
-#     primes = []
-#     for possiblePrime in range(2, num+1):
-#     # Assume number is prime until shown it is not.
-#         isPrime = True
-#         for num in range(2, possiblePrime):
-#             if possiblePrime % num == 0:
-#                 isPrime = False
-#         if isPrime:
-#             primes.append(possiblePrime)
-#     return primes
-#
-# def addList(l):
-#     addition = 0
-#     for ele in l:
-#         addition += ele
-#     return addition
+def possiblePrimeArray(num):
+    primes = []
+    for possiblePrime in range(2, num+1):
+    # Assume number is prime until shown it is not.
+        isPrime = True
+        for num in range(2, possiblePrime):
+            if possiblePrime % num == 0:
+                isPrime = False
+        if isPrime:
+            primes.append(possiblePrime)
+    return primes
+
+def addList(l):
+    addition = 0
+    for ele in l:
+        addition += ele
+    return addition
 
 
 @app.route('/prime-sum', methods=['POST'])
@@ -59,8 +59,6 @@ def evaluatePrimeSum():
                     temp.append(primes[i])
                 i+=1
         count -= 1
-
-    # result = None
 
     logging.info("My result :{}".format(result))
     return jsonify(result);
